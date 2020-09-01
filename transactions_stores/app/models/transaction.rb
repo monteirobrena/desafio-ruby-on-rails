a943d9d04cf80789ad4a21f72d33de1a7e25c926
Brena Monteiro <brena@imusics.com>
2020-09-01 05:38:49 -0300

    Add transaction tests.

--- transactions_stores/app/models/transaction.rb
+++ transactions_stores/app/models/transaction.rb
@@ -1,4 +1,6 @@
 class Transaction < ApplicationRecord
+  validates :store, :amount, :recipient, :occurred_in, :transaction_type, presence: true
+
   belongs_to :transaction_type
   belongs_to :recipient
   belongs_to :store
