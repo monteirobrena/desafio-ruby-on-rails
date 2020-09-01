868e822289c0b6bbdd27cc5dcdb89a5aeb443743
Brena Monteiro <brena@imusics.com>
2020-09-01 02:30:46 -0300

    Upload CNAB file to tmp/storage.

--- transactions_stores/app/controllers/financial_transactions_controller.rb
+++ transactions_stores/app/controllers/financial_transactions_controller.rb
@@ -1,12 +1,12 @@
 class FinancialTransactionsController < ApplicationController
   def create
-    file_upload(params[:cnab_file]
+    file_upload(params[:cnab_file])
   end
 
   private
 
   def file_upload(file)
-    File.open(File.join('tmp', file.original_filename), 'wb') do |f|
+    File.open(File.join('tmp', 'storage', file.original_filename), 'wb') do |f|
       f.write(file.read)
     end
   end
