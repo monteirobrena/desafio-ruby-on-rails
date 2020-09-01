67baff29dc04bdd29e2d6395a10eb56737920a6e
Brena Monteiro <brena@imusics.com>
2020-09-01 02:21:24 -0300

    Add route and method to upload file.

--- transactions_stores/app/controllers/financial_transactions_controller.rb
+++ transactions_stores/app/controllers/financial_transactions_controller.rb
@@ -1,2 +1,13 @@
 class FinancialTransactionsController < ApplicationController
+  def create
+    file_upload(params[:cnab_file]
+  end
+
+  private
+
+  def file_upload(file)
+    File.open(File.join('tmp', file.original_filename), 'wb') do |f|
+      f.write(file.read)
+    end
+  end
 end
